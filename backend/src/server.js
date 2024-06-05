b72bd1ec96c4a5f5bf561a5b3fa4c17ccf8f0a32
bojan.spasic <bojan.spasic@united.cloud>
2024-06-05 20:33:13 +0200

    Added getting chocos by factory

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,6 +15,7 @@ app.get("/", (req, res) => {
 
 // Load data
 const factories = JSON.parse(fs.readFileSync("./data/factories.json", "utf-8"));
+const chocolates = JSON.parse(fs.readFileSync("./data/chocolates.json", "utf-8"));
 
 // Factories routes
 app.post("/factories", (req, res) => {
@@ -27,7 +28,6 @@ app.post("/factories", (req, res) => {
     logo,
     managerUsername,
     rating: 0,
-    chocolates: [],
     deleted: false,
   };
   factories.push(newFactory);
@@ -57,6 +57,80 @@ app.delete("/factories/:name", (req, res) => {
   factory.deleted = true;
   fs.writeFileSync("./data/factories.json", JSON.stringify(factories, null, 2));
   res.send("Factory deleted");
+});
+
+// Chocolates routes
+app.post("/chocolates", (req, res) => {
+  const { name, price, type, factoryName, kind, weight, description, image, status, quantity } = req.body;
+  const newChocolate = {
+    name,
+    price,
+    type,
+    factoryName,
+    kind,
+    weight,
+    description,
+    image,
+    status,
+    quantity,
+    deleted: false,
+  };
+  chocolates.push(newChocolate);
+  fs.writeFileSync("./data/chocolates.json", JSON.stringify(chocolates, null, 2));
+  res.status(201).send(newChocolate);
+});
+
+app.get("/chocolates", (req, res) => {
+  const activeChocolates = chocolates.filter((c) => !c.deleted);
+  res.send(activeChocolates);
+});
+
+app.get("/chocolates/:name", (req, res) => {
+  const chocolate = chocolates.find((c) => c.name === req.params.name && !c.deleted);
+  if (!chocolate) {
+    return res.status(404).send("Chocolate not found");
+  }
+  res.send(chocolate);
+});
+
+// New route: Get all chocolates in a specific factory
+app.get("/factories/:name/chocolates", (req, res) => {
+  const factory = factories.find((f) => f.name === req.params.name && !f.deleted);
+  if (!factory) {
+    return res.status(404).send("Factory not found");
+  }
+  const factoryChocolates = chocolates.filter((c) => c.factoryName === req.params.name && !c.deleted);
+  res.send(factoryChocolates);
+});
+
+app.put("/chocolates/:name", (req, res) => {
+  const chocolate = chocolates.find((c) => c.name === req.params.name && !c.deleted);
+  if (!chocolate) {
+    return res.status(404).send("Chocolate not found");
+  }
+  const { price, type, kind, weight, description, image, status, quantity } = req.body;
+  if (price) chocolate.price = price;
+  if (type) chocolate.type = type;
+  if (kind) chocolate.kind = kind;
+  if (weight) chocolate.weight = weight;
+  if (description) chocolate.description = description;
+  if (image) chocolate.image = image;
+  if (status) chocolate.status = status;
+  if (quantity) chocolate.quantity = quantity;
+
+  fs.writeFileSync("./data/chocolates.json", JSON.stringify(chocolates, null, 2));
+  res.send("Chocolate updated");
+});
+
+// Logical delete for chocolates
+app.delete("/chocolates/:name", (req, res) => {
+  const chocolate = chocolates.find((c) => c.name === req.params.name);
+  if (!chocolate) {
+    return res.status(404).send("Chocolate not found");
+  }
+  chocolate.deleted = true;
+  fs.writeFileSync("./data/chocolates.json", JSON.stringify(chocolates, null, 2));
+  res.send("Chocolate deleted");
 });
 
 app.listen(port, () => {
